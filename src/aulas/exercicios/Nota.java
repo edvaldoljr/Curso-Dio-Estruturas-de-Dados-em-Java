a331df9f0cc317cb68c03d226948357ae373bc39
edvaldoljr <edvaldoluizj@gmail.com>
2021-12-17 14:07:28 -0300

    Exercício Nota.

---
+++ src/aulas/exercicios/Nota.java
@@ -0,0 +1,27 @@
+package aulas.exercicios;
+
+import java.util.Scanner;
+
+/**
+ *Construa um algoritmo que peça uma nota entre zero e dez.
+ * Mostre uma mensagem caso valor estiver invalido e
+ * continue, pedindo até que o usuário informe um valor válido.
+ **/
+public class Nota {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int n;
+
+        System.out.println("Digite um valor entre zero e dez: ");
+        n = scan.nextInt();
+
+        while (n < 0 || n > 10 ) {
+            System.out.println("Nota inválida digite novamente: ");
+            n = scan.nextInt();
+        }
+        System.out.println("Valor válido!");
+    }
+}
